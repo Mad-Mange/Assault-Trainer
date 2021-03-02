96b1a142eb232f226877f4aac37bdc5f252213d4
Magnus Eriksson <39713545+Mad-Mange@users.noreply.github.com>
2021-03-03 00:04:27 +0100

    Added class employee

--- Assault Trainer.cpp
+++ Assault Trainer.cpp
@@ -3,18 +3,57 @@
 #include <cwchar>
 #include "proc.h"
 
-class Employee {
+using std::string;
+using std::cout;
+using std::endl;
 
+class AbstractEmployee {
+    void AskForPromotion();
 };
 
-int main()
+class Employee {
+     string Name;
+     string Company;
+     int Age;
+
+public:
+    void setName(string name) { Name = name; }
+    string getName() { return Name; }
+    void setCompany(string comp) { Company = comp; }
+    string getCompany() { return Company; }
+    void setAge(int age) {
+        if(age >= 18)
+        Age = age;
+    }
+    int getAge() { return Age; }
+
+    Employee(string name, string comp, int age) { Name = name; Company = comp; Age = age; }
+
+     void presentation() {
+         cout << "My name is " << Name << " and working at " << 
+             this->Company << " and Im " << this->Age << " old.\n";
+     }
+};
+
+int main(int argc, char** argv)
 {
-    Employee emp;
+    Employee emp = Employee("Mange","Crime AB", 49);
+    Employee emp1 = Employee("Lisa", "El o Vatten", 21);
+    Employee emp3 = Employee("Anders", "Bull & Bång", 35);
+
+
+
+    emp.presentation();
+    emp1.presentation();
+    emp3.setName("Bullen");
+    cout << "Hämtar namn med getter!! " << emp3.getName() << endl;
+    emp3.presentation();
+
 
     // Get proc id
     const wchar_t* namn = L"Notepad";
     DWORD processID = GetProcId(L"Notepad");
-    std::cout << "ProcessId: " << processID << "\n";
+    cout << "ProcessId: " << processID << "\n";
 
     // Get module base adddress
 
